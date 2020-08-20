5f1883feb98e65b675eee295df30b141f178cfcd
Schweini-PEK <fengzheshi@gmail.com>
2020-08-20 13:44:57 -0700

    .mat file processor added

--- preprocessor.py
+++ preprocessor.py
@@ -1,13 +1,18 @@
 import argparse
 import csv
+import logging
 import pickle
+import random
 from os import listdir
 from os.path import join
 
+import scipy.io
 import torch
 
+logger = logging.getLogger(__name__)
 
-def preprocessor(src, trg, name, test=False):
+
+def processor(src, trg, name, type, test=False):
     """Load and save training/test data from dir.
 
     Get all the csv files from a dir. Typically the names of the files are their alphas.
@@ -19,52 +24,70 @@ def preprocessor(src, trg, name, test=False):
     """
     samples = []
     n_a, n_x = 0, 0
-    for f_path in listdir(src):
-        alpha = list(map(float, f_path[:-4].split('_')[2::2]))
-        n_a = len(alpha)
+    if type == 'csv':
+        for f_path in listdir(src):
+            alpha = list(map(float, f_path[:-4].split('_')[2::2]))
+            n_a = len(alpha)
 
-        with open(join(src, f_path), 'r') as f:
-            reader = csv.reader(f)
+            with open(join(src, f_path), 'r') as f:
+                reader = csv.reader(f)
 
-            if test:
-                sample = []
-                for line in reader:
-                    sample.append(list(map(float, line[1:])))
-                samples.append([alpha, sample])
-                n_x = len(sample[0])
+                if test:
+                    sample = []
+                    for line in reader:
+                        sample.append(list(map(float, line[1:])))
+                    samples.append([alpha, sample])
+                    n_x = len(sample[0])
 
-            else:
-                x_prior = list(map(float, next(reader)[1:]))
-                n_x = len(x_prior)
-                for line in reader:
-                    try:
-                        x_future = list(map(float, line[1:]))
-                        samples.append([torch.tensor(x_prior + alpha),
-                                        torch.tensor(x_future)])
-                        x_prior = x_future
-                    except ValueError as e:
-                        print('In file {}, check the line: {}'.format(f_path, line))
-                        raise
+                else:
+                    x_past = list(map(float, next(reader)[1:]))
+                    n_x = len(x_past)
+                    for line in reader:
+                        try:
+                            x_future = list(map(float, line[1:]))
+                            samples.append([torch.tensor(x_past + alpha),
+                                            torch.tensor(x_future)])
+                            x_past = x_future
+                        except ValueError as e:
+                            print('In file {}, check the line: {}'.format(f_path, line))
+                            raise
+
+    elif type == 'mat':
+        if not test:
+            data = scipy.io.loadmat(src)
+            x_raw = data['Xdata']
+            y_raw = data['Ydata']
+            index = list(range(len(x_raw)))
+            random.shuffle(index)
+            for i in index[:1000]:
+                samples.append([torch.tensor(x_raw[i]), torch.tensor(y_raw[i])])
+
+            n_x = len(y_raw[0])
+            n_a = len(x_raw[0]) - n_x
 
     if name is None:
+        name = src.split('/')[-1].split('.')[0] + '_x{}a{}'.format(n_x, n_a)
         if test:
-            name = trg + 'NS_truth_x{}a{}.pkl'.format(n_x, n_a)
-
+            name = 'test_' + name
         else:
-            name = trg + 'data_{}_x{}a{}.pkl'.format(len(samples), n_x, n_a)
-    else:
-        name = trg + name + '.pkl'
-    with open(name, 'wb') as f_save:
+            name += '_{}'.format(len(samples))
+
+    pth = trg + name + '.pkl'
+    with open(pth, 'wb') as f_save:
         pickle.dump(samples, f_save)
-    print('Save {} samples at {}'.format(len(samples), name))
+    print('Save {} samples at {}'.format(len(samples), pth))
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--input", "-i", required=True, type=str, help="Input dir")
     parser.add_argument("--output", "-o", default="dataset/", type=str, help="Output dir")
-    parser.add_argument("--test", "-t", default=False, type=bool, help="Test set or not")
+    parser.add_argument("--test", default=False, type=bool, help="Test set or not")
     parser.add_argument("--name", "-n", default=None, help="File name")
+    parser.add_argument("--type", "-t", default='csv', help='Processor type')
     args = parser.parse_args()
 
-    preprocessor(args.input, args.output, args.name, args.test)
+    if args.type in {'csv', 'mat'}:
+        processor(args.input, args.output, args.name, args.type, args.test)
+    else:
+        raise NameError('Wrong file type: {}'.format(args.type))
---
+++ visualizer.py
@@ -0,0 +1,36 @@
+import argparse
+import logging
+
+import matplotlib.pyplot as plt
+
+from utils import analysis
+from utils import kits
+
+logging.basicConfig(level=logging.INFO)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--save", "-s", default=False, type=bool, help="Save figure")
+    parser.add_argument("--model", "-m", help="Model for predicting")
+    parser.add_argument("--dataset", "-d", default='dataset/NS_truth_x3a5.pkl', help="Test set")
+    parser.add_argument("--loss_path", help="Path of Ray json file")
+    parser.add_argument("--model_dir", default='results/compare', help="Path of Ray json file")
+    parser.add_argument("--loss_dir", default='results/losses1', help="Path of Ray json file")
+    args = parser.parse_args()
+
+    models_pth = kits.get_pth_from_dir(args.model_dir)
+    for pth in models_pth:
+        analysis.forecast(model_path=pth, truth_path=args.dataset, save_fig=args.save)
+
+    losses_pth = kits.get_pth_from_dir(args.loss_dir)
+    fig, ax = plt.subplots()
+    scalar_map = kits.color_wheel(len(losses_pth), theme='prism')
+    for i, pth in enumerate(losses_pth):
+        loss = analysis.get_loss_from_ray(pth)
+        color = scalar_map.to_rgba(i)
+        ax.plot(loss, color=color, label=pth.split('/')[-1].split('.')[0])
+    leg = ax.legend()
+    plt.xlabel('Iterations')
+    plt.ylabel('L1 Loss')
+    plt.title('Validation Losses Comparison')
+    plt.show()
